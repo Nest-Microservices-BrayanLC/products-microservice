ac5eaba0b2bc6ab55cf5a014e413cd195c31439d
brayan <brayanlc82@gmail.com>
2024-09-30 22:46:19 -0500

    Switch Products Service to Microservice Architecture
    Converted the Products service to use microservice patterns. Updated DTO validation and modified the service methods to utilize message patterns. Changes include adding required dependencies and configuring the TCP transport in the main module.

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(envs.port);
-  logger.log(`Server running on http://localhost:${envs.port}`);
+  await app.listen();
+  logger.log(`Products Microservice is running on: ${envs.port}`);
 }
 bootstrap();
